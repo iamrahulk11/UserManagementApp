ebb820c3101168c8f73f579ba15a3fec53a85664
EENADU <zero21code@gmail.com>
2023-03-06 17:18:42 +0530

    Modified

--- app/src/main/java/com/example/usermanagement/SecondActivity.kt
+++ app/src/main/java/com/example/usermanagement/SecondActivity.kt
@@ -12,7 +12,7 @@ import com.google.gson.Gson
 
 class SecondActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivitySecondBinding
-    val backHomePage = Intent(this, MainActivity::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -33,6 +33,7 @@ class SecondActivity : AppCompatActivity(), View.OnClickListener {
         alertDialogBuilder.setTitle("Confirmation")
         alertDialogBuilder.setMessage("Do you want to return to home page?")
         alertDialogBuilder.setPositiveButton("Yes") { Dialog, which ->
+            val backHomePage = Intent(this, MainActivity::class.java)
             startActivity(backHomePage)
             finish()
         }
@@ -48,7 +49,7 @@ class SecondActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btnclear ->{
-
+                val backHomePage = Intent(this, MainActivity::class.java)
                 startActivity(backHomePage)
                 finish()
             }
